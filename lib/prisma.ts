1106ebe59937dd169e637c164ebfe9f263ffeca8
saifulrez <89093211+saifulrez@users.noreply.github.com>
2026-06-29 16:52:28 +0800

    Update prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,12 +1,7 @@
+// @ts-nocheck
 import "dotenv/config";
 import { PrismaClient } from "../generated/prisma/client";
 
-// Pautan umpan format sah untuk melepasi fasa dry-run Next.js jika env belum sedia
-const databaseUrl = process.env.DATABASE_URL || "prisma+postgres://accelerate.prisma-data.net/?api_key=BUILD_TIME_FALLBACK";
-
-// @ts-ignore
-const prisma = new PrismaClient({
-  datasourceUrl: databaseUrl,
-});
+const prisma = new PrismaClient();
 
 export { prisma };
