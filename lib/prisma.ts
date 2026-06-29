45b1ef8cf0c7d799d76b83f0de70ec526c4b16ac
saifulrez <89093211+saifulrez@users.noreply.github.com>
2026-06-29 15:55:06 +0800

    Update prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,7 @@
 import "dotenv/config";
-import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../generated/prisma/client";
 
-const connectionString = `${process.env.DATABASE_URL}`;
-const adapter = new PrismaPg({ connectionString });
-const prisma = new PrismaClient({ adapter });
+// Prisma Postgres berjalan secara 'native' tanpa memerlukan adapter-pg
+const prisma = new PrismaClient();
 
 export { prisma };
