b0cf85ef3d1574bd15b2f5defbe3600cc450a4a2
saifulrez <89093211+saifulrez@users.noreply.github.com>
2026-06-30 09:32:38 +0800

    Update prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,7 +1,7 @@
 import { PrismaClient } from "../generated/prisma/client";
 
-// Kita masukkan {} as any untuk memenuhi syarat wajib 1 argumen daripada TypeScript.
-// Memandangkan DATABASE_URL sudah ada di Vercel Env, Prisma akan gunakannya secara automatik!
+// Kita masukkan {} as any di dalam kurungan untuk memenuhi syarat wajib 1 argumen.
+// Objek kosong ini akan mengelakkan ralat Prisma, manakala 'as any' akan mendiamkan TypeScript!
 const prisma = new PrismaClient({} as any);
 
 export { prisma };
