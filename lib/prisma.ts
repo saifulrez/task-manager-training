17f9d75e4bcd3d13cb72443385d7ec4e2480d08d
saifulrez <89093211+saifulrez@users.noreply.github.com>
2026-06-30 09:23:43 +0800

    Update prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,15 @@
 import { PrismaClient } from "../generated/prisma/client";
 
-const prisma = new PrismaClient();
+let prismaInstance: any;
+
+const prisma = new Proxy({} as any, {
+  get(target, prop) {
+    if (!prismaInstance) {
+      // Kita hantar {} as any untuk memenuhi syarat 1 argumen tanpa sekatan TypeScript
+      prismaInstance = new PrismaClient({} as any);
+    }
+    return prismaInstance[prop];
+  },
+});
 
 export { prisma };
