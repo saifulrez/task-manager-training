70b8fa66dcfba4ccacd66b5e93ab99edcc04d877
saifulrez <89093211+saifulrez@users.noreply.github.com>
2026-06-30 09:04:52 +0800

    Update prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,19 +1,5 @@
-// @ts-nocheck
 import { PrismaClient } from "../generated/prisma/client";
 
-let prisma: PrismaClient;
-
-// Jika tiada DATABASE_URL (semasa fasa 'next build' di Vercel),
-// kita cipta Proxy palsu supaya Next.js boleh lepas fasa build dengan gembira.
-if (!process.env.DATABASE_URL) {
-  prisma = new Proxy({} as any, {
-    get: () => new Proxy({} as any, {
-      get: () => () => Promise.resolve([]),
-    }),
-  });
-} else {
-  // Apabila aplikasi sudah 'live' di internet, ia akan guna PrismaClient asli dengan lancar!
-  prisma = new PrismaClient();
-}
+const prisma = new PrismaClient();
 
 export { prisma };
