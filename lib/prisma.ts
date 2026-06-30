a5bfed205964c444a6d22b06411f1afa3287e848
saifulrez <89093211+saifulrez@users.noreply.github.com>
2026-06-30 09:49:31 +0800

    Update prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -3,5 +3,5 @@ import { PrismaClient } from "../generated/prisma/client";
 // Kita masukkan {} as any di dalam kurungan untuk memenuhi syarat wajib 1 argumen.
 // Objek kosong ini akan mengelakkan ralat Prisma, manakala 'as any' akan mendiamkan TypeScript!
 const prisma = new PrismaClient({} as any);
-
+// Ujian Binaan Baru
 export { prisma };
